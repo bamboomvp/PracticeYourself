fb436716c0157f55b4f718cae0b604476cf69692
Di@Bamboo-PC <illidan.bao@gmail.com>
2017-01-22 19:36:07 -0500

    create and upload a new java src file

---
+++ PracticeYourself/src/baodavi/TryItOut.java
@@ -0,0 +1,11 @@
+package baodavi;
+
+import java.util.ArrayList;
+
+public class TryItOut {
+	public static void main(String[] args){
+		Class<?> what = int.class;
+		Class<?> is = ArrayList.class;
+		Class<Double> aaa = Double.class;
+	}
+}
